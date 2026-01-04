ccf6bc336596cc71bc767f8f34fa1a1b2a887cd4
Mokshith Kummari <95648601+Mokshith77@users.noreply.github.com>
2026-01-04 11:03:16 +0000

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,4 @@
+RAW_DATA_PATH = "data/raw/risk_data.csv"
+PROCESSED_DATA_PATH = "data/processed/processed_data.csv"
+CURATED_DATA_PATH = "data/curated/curated_data.csv"
+MODEL_PATH = "data/curated/model.pkl"
